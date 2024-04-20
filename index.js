ff75b6e80565155d672a94bd2923045b43c09bbf
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2024-04-20 19:29:05 +0300

    asdasd

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import http from 'http';
 import path from 'path';
 import fs from 'fs';
+import { Server } from 'socket.io';
+
 
 const __dirname = path.resolve();
 
@@ -10,6 +12,8 @@ const pathToStyle = path.join(__dirname, 'static', 'style.css');
 const styleFile = fs.readFileSync(pathToStyle);
 const pathToScript = path.join(__dirname, 'static', 'script.js');
 const scriptFile = fs.readFileSync(pathToScript);
+const pathToScriptIo = path.join(__dirname, 'static', 'socket.io.min.js');
+const scriptFileIo = fs.readFileSync(pathToScriptIo);
 const server = http.createServer((req, res) => {
     try {
         if (req.url == '/' && req.method === 'GET') {
@@ -20,6 +24,9 @@ const server = http.createServer((req, res) => {
         }
         if (req.url == '/style.css' && req.method === 'GET') {
             return res.end(styleFile)
+        }
+        if (req.url == '/socket.io.min.js' && req.method === 'GET') {
+            return res.end(scriptFileIo)
         }
         res.writeHead(404, "Not Found")
         return res.end()
@@ -32,4 +39,16 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000, function () {
     console.log('server is running on port 3000')
+})
+const io = new Server(server);
+io.on('connection', (socket) => {
+    console.log(`a user connected. id${socket.id}`)
+    let username = ""
+    socket.on("set_nickname", (data) => {
+        username = data
+    })
+    socket.on('new_message', (data) => {
+        io.emit("message", username + ": " + data)
+    })
+
 })
--- static/script.js
+++ static/script.js
@@ -1,1 +1,26 @@
-alert("welcome to the club")
+let name = "user"
+
+
+let socket = io();
+document.querySelector('#form').addEventListener('submit', function (e) {
+    e.preventDefault()
+    let message = document.getElementById('input').value
+    if (message.length) {
+        socket.emit('new_message', message)
+        document.getElementById('input').value = ""
+    }
+})
+
+socket.on("message", (data) => {
+    let item = document.createElement('li')
+    item.textContent = data
+    document.getElementById('messages').appendChild(item)
+    window.scrollTo(0, document.body.scrollHeight);
+})
+
+document.getElementById('cn').addEventListener('click', () => {
+    name = prompt('Enter new nickname')
+    socket.emit("set_nickname", name)
+})
+socket.emit("set_nickname", name)
+
